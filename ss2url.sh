d7e7eb5b68daa8750eba14c2933f5d61b64c6802
TJ Luoma <github@tjluoma.com>
2014-12-10 15:53:57 -0500

    Added more comments for readability

--- ss2url.sh
+++ ss2url.sh
@@ -6,47 +6,85 @@
 # Web: 	http://RhymesWithDiploma.com
 # Date:	2014-12-10
 
+# You MUST set the first 3 variables, otherwise there's no hope of this working.
+
+	# This is the Remote HOSTname of the server where you will be scp'ing to
+## MUST SET ##
+RHOST='mini.luo.ma'
+
+	# This is the folder on the $RHOST
+	# Note that the path should be relative to your $HOME
+## MUST SET ##
+RDIR='Dropbox/images.luo.ma/ss'
+
+	# What is the public URL for the RDIR folder?
+## MUST SET ##
+URL_ROOT='http://images.luo.ma/ss'
+
+
+##########################################################################################
+##########################################################################################
+#
+# You do not _need_ to change any of these 4 variables, but you might _want_ to
+
+	# Which app should growlnotify use for the icon when displaying alerts
+	# 	I still use and like the original Skitch, so I use some of its sounds
+	# 	and its app icon. If you want, you can download it from here:
+	# 		http://evernote.com/download/get.php?file=SkitchMac_v1
+GROWL_APP='Skitch'
+
+	# What sound should be played when the scp starts?
+	# This can be any sound file on your Mac (as long as it can be played with `afplay`)
+	# If it doesn't exist it will be quietly ignored
+START_SND='/Applications/Skitch.app/Contents/Resources/PTW_commence.m4a'
+
+	# What sound should be played if the scp succeeds?
+	# This can be any sound file on your Mac (as long as it can be played with `afplay`)
+	# If it doesn't exist it will be quietly ignored
+SUCCESS_SND='/Applications/Skitch.app/Contents/Resources/PTW_complete.m4a'
+
+	# What sound should be played if the scp fails?
+	# This can be any sound file on your Mac (as long as it can be played with `afplay`)
+	# If it doesn't exist it will be quietly ignored
+FAIL_SND='/System/Library/Sounds/Sosumi.aiff'
+
+
+##########################################################################################
+##########################################################################################
+##########################################################################################
+#
+#
+# You should not change anything below here unless you really know what you are doing.
+# But you are welcome to look through. There are lots of comments to help you follow my
+# thought patterns.
+
 zmodload zsh/stat
 zmodload zsh/datetime
 
 NAME="$0:t:r"
 
-	# This is the Remote HOSTname of the server where you will be scp'ing to
-RHOST='mini.luo.ma'
-
-	# This is the folder on the $RHOST
-	# Note that the path should be relative to your $HOME
-RDIR='Dropbox/images.luo.ma/ss'
-
-	# What is the public URL for the RDIR folder?
-URL_ROOT='http://images.luo.ma/ss'
-
-	# What sound should be played when the scp starts?
-START_SND='/Applications/Skitch.app/Contents/Resources/PTW_commence.m4a'
-
-	# What sound should be played if the scp succeeds?
-SUCCESS_SND='/Applications/Skitch.app/Contents/Resources/PTW_complete.m4a'
-
-	# What sound should be played if the scp fails?
-FAIL_SND='/System/Library/Sounds/Sosumi.aiff'
-
+	# Check to see if the user has changed the default name which is prefixed to screenshots
 PREFIX=`defaults read com.apple.screencapture name 2>/dev/null`
 
+		# If no, use the default
 	[[ "$PREFIX" == "" ]] && PREFIX='Screen Shot'
 
+	# Has the user changed the KIND of image which will be used for screenshots?
+	# Generally this might be set to JPG instead of PNG
 SUFFIX=`defaults read com.apple.screencapture type 2>/dev/null`
 
+		# if no, use the default
 	[[ "$SUFFIX" == "" ]] && SUFFIX='png'
 
 ##########################################################################################
 
-	# if the
+	# if the file needs to be renamed, this is the function which will do it
 function rename_image {
 
 		# Get the file extension
 	EXT="$i:e:l"
 
-		# get the ctime (creation time)
+		# get the ctime (creation time) of the file
 	EPOCH_TIMESTAMP=$(zstat -L +ctime "$i")
 
 		# reformat the creation time into something more readable
@@ -64,7 +102,10 @@ function rename_image {
 }
 
 ##########################################################################################
-
+##
+## CASE 1 - NO ARGS = the script has been called with no arguments.
+##				So we assume we need to check the default folder for screenshots
+##				and process any that are found there
 
 if [ "$#" = "0" ]
 then
@@ -79,12 +120,24 @@ then
 	cd "$DIR"
 
 		# find any files matching the screenshot filename and run this program on it
+		## which will send us to the 'Case 2' below
+		## Note the 'maxdepth 0' which prevents us from having to check every sub-folder
+		## Also note that by limiting it to the LOGNAME of the user who called this script
+		## we can use '/tmp/' directory without worrying too much about some jokester
+		## putting properly named files in that directory to upload them to our server.
+		## It's not a lot of security, but it's better than none.
 	find * -maxdepth 0 -user "$LOGNAME" -iname "${PREFIX} *\.${SUFFIX}" -exec "$0" {} \; 2>/dev/null
 
 		# We're done
 	exit 0
 fi
 
+##########################################################################################
+##########################################################################################
+##
+## CASE 2 - ARGS = the user has called the script with 1 or more arguments, presumably
+##					these are image files that the user wants to upload
+##
 
 for i in "$@"
 do
@@ -128,7 +181,7 @@ do
 
 			# post a Growl notification that we're uploading the file
 		growlnotify --sticky \
-			--appIcon "Skitch" --identifier "$SHORT" \
+			--appIcon "$GROWL_APP" --identifier "$SHORT" \
 			--message "$SHORT" --title "$NAME: Uploading"
 
 			# upload the file
